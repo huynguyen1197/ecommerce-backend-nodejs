d4cf5ae94bb8b758e8ba56e94741c968d71ac31c
Huy Nguyen <huynguyenvn97@gmail.com>
2024-10-21 23:13:41 -0500

    init project

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const compression = require("compression");
+const express = require("express");
+const { default: helpmet } = require("helmet");
+const morgan = require("morgan");
+const app = express();
+
+// init middlewares
+app.use(morgan("dev"));
+app.use(helpmet());
+app.use(compression());
+
+// init db
+
+// init routes
+app.get("/", (req, res, next) => {
+	const msg = "test msg abc";
+	return res.status(200).json({
+		message: msg,
+		metadata: msg.repeat(1000000),
+	});
+});
+
+// handle error
+
+module.exports = app;
