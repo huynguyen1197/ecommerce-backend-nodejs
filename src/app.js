d6d1145da64b1d1c951756ef45253ecbc95aaf25
Huy Nguyen <huynguyenvn97@gmail.com>
2024-10-24 23:33:59 -0500

    fix typo

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const compression = require("compression");
 const express = require("express");
-const { default: helpmet } = require("helmet");
+const { default: helmet } = require("helmet");
 const morgan = require("morgan");
 const app = express();
 
 // init middlewares
 app.use(morgan("dev"));
-app.use(helpmet());
+app.use(helmet());
 app.use(compression());
 
 // init db
